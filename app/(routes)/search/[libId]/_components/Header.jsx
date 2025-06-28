5cbddf5847c5d758cb00410227d3081c4490c525
HatimOO7 <hatimchowdhury80@gmail.com>
2025-06-28 23:31:46 +0600

    header section

---
+++ app/(routes)/search/[libId]/_components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = ({ searchInputRecord }) => {
+  return (
+    <div>
+      <h1>Search Results</h1>
+    </div>
+  )
+}
+
+export default Header
--- app/(routes)/search/[libId]/page.jsx
+++ app/(routes)/search/[libId]/page.jsx
@@ -1,16 +1,36 @@
 "use client";
 import React from 'react'
 import { useParams } from 'next/navigation';
+import { useEffect } from 'react';
+import { supabase } from '@/services/superbase';
+
+
 
 
 
 function SearchQueryResult() {
     const { libId } = useParams();
+    const [searchInputRecord, setSearchInputRecord] = useState();
     console.log(libId);
+
+    useEffect(() => {
+        GetSearchQueryRecord();
+    }, []);
+
+    const GetSearchQueryRecord = async () => {
+
+        let { data: Library, error } = await supabase
+            .from('Library')
+            .select('*,Chats(*)')
+            .eq('libId', libId);
+
+        setSearchInputRecord(Library[0]);
+    }
+
   return (
     
     <div>
-      SearchQueryResult
+       <Header searchInputRecord={searchInputRecord} />
     </div>
   )
 }
