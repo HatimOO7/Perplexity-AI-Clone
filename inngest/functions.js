5ce2b1a0480fd060f891702c570bd9e4e39c413a
HatimOO7 <hatimchowdhury80@gmail.com>
2025-07-09 22:22:30 +0600

    implemented inngest for using llm api

---
+++ inngest/functions.js
@@ -0,0 +1,48 @@
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "1s");
+    return { message: `Hello ${event.data.email}!` };
+  },
+);
+
+export const llmModel = inngest.createFunction(
+    { id: 'llm-model' },
+    { event: 'llm-model' },
+    async ({ event, step }) => {
+        const aiResp = await step.ai.infer('generate-ai-llm-model-call', {
+            model: step.ai.models.gemini({
+                // model: 'gemini-2.0-flash-exp-image-generation',
+                model: 'gemini-1.5-flash',
+                apiKey: ''
+            }),
+            body: {
+                contents: [
+                    {
+                        // role: 'system',
+                        role: 'system',
+                        parts: [
+                            {
+                                text: 'Depends on user input sources, Summarize and search about topic, Give me markdown text with proper formatting. User Input is:'
+                                    + event.data.searchInput
+                            }
+                        ]
+                    },
+                    {
+                        role: "user",
+                        parts: [
+                            {
+                                text: JSON.stringify(event.data.searchResult)
+                            }
+                        ]
+                    }
+                ]
+            }
+        })
+
+        
+    }
+)
